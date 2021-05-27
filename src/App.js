99d6f962bcd7752fa21dffd8c76d3c3c522a1ed6
Jake Flynn <JakeFlynn@Jakes-MBP-4.attlocal.net>
2021-05-27 16:11:43 -0400

    home page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,22 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
+        <Switch>
+          <Route path="/" exact>
+            <Home
+            name={name}
+            setName={setName}
+            fetchQuestions={fetchquestions}
+            />
+          </Route>
+        </Switch>
       </div>
       <Footer />
     </BrowserRouter>
