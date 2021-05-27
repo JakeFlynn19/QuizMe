51a25690a01adf0f347f86143c1648711cdbce88
Jake Flynn <JakeFlynn@Jakes-MBP-4.attlocal.net>
2021-05-27 13:30:39 -0400

    questions

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 import { useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer"
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-      <Header />
-    </div>
-    <Footer />
+      <div className="App">
+        <Header />
+      </div>
+      <Footer />
     </BrowserRouter>
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,17 @@
 const Footer = () => {
-    return (
-      <div
-        style={{
-          textAlign: "center",
-          marginBottom: 10,
-        }}
-      >
-        Created by{" "}
-        <a
-          href="https://www.jakeflynn.tech/"
-          style={{ cursor: "pointer" }}
-        >
-          Jake Flynn
-        </a>
-      </div>
-    );
-  };
-  
-  export default Footer;
-  
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Created by{" "}
+      <a href="https://www.jakeflynn.tech/" style={{ cursor: "pointer" }}>
+        Jake Flynn
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
