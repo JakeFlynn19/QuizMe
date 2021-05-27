0eb6562a3a3ca675532c282294d1b690efa89242
Jake Flynn <JakeFlynn@Jakes-MBP-4.attlocal.net>
2021-05-27 13:21:13 -0400

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import axios from "axios";
+import { useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.css';
+import Header from "./components/Header/Header";
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      
+      <Header />
     </div>
+    </BrowserRouter>
   );
 }
 
